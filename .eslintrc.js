5de59df630322b968a2e2d3512c30f45cdaafba3
wukaiwei1 <2596862515@qq.com>
2022-07-13 09:39:53 +0800

    项目初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,11 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
     parser: '@babel/eslint-parser'
+    // template第一行报错
+    // requireConfigFile: false
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
